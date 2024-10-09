16fb7e9f89bf34b08ba09befbc0463dea9cb73e8
Lucia Pigliacampi <lucia.pigliacampi90@gmail.com>
2024-10-09 11:56:03 -0300

    cambios en backend

--- Back/index.js
+++ Back/index.js
@@ -1,9 +1,11 @@
 import express from "express"
 import mysql2 from "mysql2"
+import cors from "cors"
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 
 const connection = mysql2.createConnection({
@@ -27,7 +29,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/cliente', (req, res) => {
-  connection.query('SELECT * FROM cliente', (err, results) => {
+  connection.query('SELECT c.*, b.nombre as nombre_barrio, l.nombre as nombre_localidad, t.nombre as nombre_tipo_cliente, td.nombre as nombre_tipo_documento FROM cliente as c inner join barrio as b on b.id_barrio = c.id_barrio inner join localidad as l on l.id_localidad = c.id_localidad inner join tipocliente as t on t.id_tipo_cliente = c.id_tipo_cliente inner join tipodocumento as td on td.id_tipo_documento = c.id_tipo_documento WHERE c.estado=1 OR c.estado is null ORDER BY c.nombre asc', (err, results) => {
       if (err) {
           return res.status(500).json({ error: err.message });
       }
@@ -35,144 +37,144 @@ app.get('/cliente', (req, res) => {
   });
 });
 
-app.post('/cliente', (req, res) => {
-  const { nombre, apellido, telefono, direccion, id_barrio, id_localidad, correo_electronico, id_tipo_cliente, id_tipo_documento, numero_documento } = req.body;
-  connection.query(
-      'INSERT INTO cliente (nombre, apellido, telefono, direccion, id_barrio, id_localidad, correo_electronico, id_tipo_cliente, id_tipo_documento, numero_documento) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-      [nombre, apellido, telefono, direccion, id_barrio, id_localidad, correo_electronico, id_tipo_cliente, id_tipo_documento, numero_documento],
-      (err, results) => {
-          if (err) {
-              return res.status(500).json({ error: err.message });
-          }
-          res.status(201).json({ id: results.insertId });
-      }
-  );
-});
+// app.post('/cliente', (req, res) => {
+//   const { nombre, apellido, telefono, direccion, id_barrio, id_localidad, correo_electronico, id_tipo_cliente, id_tipo_documento, numero_documento } = req.body;
+//   connection.query(
+//       'INSERT INTO cliente (nombre, apellido, telefono, direccion, id_barrio, id_localidad, correo_electronico, id_tipo_cliente, id_tipo_documento, numero_documento) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+//       [nombre, apellido, telefono, direccion, id_barrio, id_localidad, correo_electronico, id_tipo_cliente, id_tipo_documento, numero_documento],
+//       (err, results) => {
+//           if (err) {
+//               return res.status(500).json({ error: err.message });
+//           }
+//           res.status(201).json({ id: results.insertId });
+//       }
+//   );
+// });
 
-app.put('/cliente', (req, res) => {
-  const { nombre, apellido, telefono, direccion, id_barrio, id_localidad, correo_electronico, id_tipo_cliente, id_tipo_documento, numero_documento } = req.body;
-  connection.query(
-      'UPDATE cliente SET nombre=?, apellido=?, telefono=?, direccion=?, id_barrio=?, id_localidad=?, correo_electronico=?, id_tipo_cliente=?, id_tipo_documento=?, numero_documento=?, estado=? WHERE id_cliente=?',
-      [nombre, apellido, telefono, direccion, id_barrio, id_localidad, correo_electronico, id_tipo_cliente, id_tipo_documento, numero_documento],
-      (err, results) => {
-          if (err) {
-              return res.status(500).json({ error: err.message });
-          }
-          res.status(201).json({ id: results.insertId });
-      }
-  );
-});
+// app.put('/cliente', (req, res) => {
+//   const { nombre, apellido, telefono, direccion, id_barrio, id_localidad, correo_electronico, id_tipo_cliente, id_tipo_documento, numero_documento } = req.body;
+//   connection.query(
+//       'UPDATE cliente SET nombre=?, apellido=?, telefono=?, direccion=?, id_barrio=?, id_localidad=?, correo_electronico=?, id_tipo_cliente=?, id_tipo_documento=?, numero_documento=?, estado=? WHERE id_cliente=?',
+//       [nombre, apellido, telefono, direccion, id_barrio, id_localidad, correo_electronico, id_tipo_cliente, id_tipo_documento, numero_documento],
+//       (err, results) => {
+//           if (err) {
+//               return res.status(500).json({ error: err.message });
+//           }
+//           res.status(201).json({ id: results.insertId });
+//       }
+//   );
+// });
 
-app.get('/producto', (req, res) => {
-  connection.query('SELECT * FROM producto', (err, results) => {
-      if (err) {
-          return res.status(500).json({ error: err.message });
-      }
-      res.json(results);
-  });
-});
+// app.get('/producto', (req, res) => {
+//   connection.query('SELECT * FROM producto', (err, results) => {
+//       if (err) {
+//           return res.status(500).json({ error: err.message });
+//       }
+//       res.json(results);
+//   });
+// });
 
-app.post('/producto', (req, res) => {
-  const { nombre, volumen, stock, id_tipo_producto } = req.body;
-  connection.query(
-      'INSERT INTO producto (nombre, volumen, stock, id_tipo_producto) VALUES (?, ?, ?, ?)',
-      [nombre, volumen, stock, id_tipo_producto],
-      (err, results) => {
-          if (err) {
-              return res.status(500).json({ error: err.message });
-          }
-          res.status(201).json({ id: results.insertId });
-      }
-  );
-});
+// app.post('/producto', (req, res) => {
+//   const { nombre, volumen, stock, id_tipo_producto } = req.body;
+//   connection.query(
+//       'INSERT INTO producto (nombre, volumen, stock, id_tipo_producto) VALUES (?, ?, ?, ?)',
+//       [nombre, volumen, stock, id_tipo_producto],
+//       (err, results) => {
+//           if (err) {
+//               return res.status(500).json({ error: err.message });
+//           }
+//           res.status(201).json({ id: results.insertId });
+//       }
+//   );
+// });
 
-app.put('/producto', (req, res) => {
-  const { nombre, volumen, stock, id_tipo_producto } = req.body;
-  connection.query(
-      'UPDATE producto SET nombre=?, volumen=?, stock=?, id_tipo_producto=?, estado=? WHERE id_producto=?',
-      [nombre, volumen, stock, id_tipo_producto],
-      (err, results) => {
-          if (err) {
-              return res.status(500).json({ error: err.message });
-          }
-          res.status(201).json({ id: results.insertId });
-      }
-  );
-});
+// app.put('/producto', (req, res) => {
+//   const { nombre, volumen, stock, id_tipo_producto } = req.body;
+//   connection.query(
+//       'UPDATE producto SET nombre=?, volumen=?, stock=?, id_tipo_producto=?, estado=? WHERE id_producto=?',
+//       [nombre, volumen, stock, id_tipo_producto],
+//       (err, results) => {
+//           if (err) {
+//               return res.status(500).json({ error: err.message });
+//           }
+//           res.status(201).json({ id: results.insertId });
+//       }
+//   );
+// });
 
-app.get('/pedido', (req, res) => {
-  connection.query('SELECT * FROM pedido', (err, results) => {
-      if (err) {
-          return res.status(500).json({ error: err.message });
-      }
-      res.json(results);
-  });
-});
+// app.get('/pedido', (req, res) => {
+//   connection.query('SELECT * FROM pedido', (err, results) => {
+//       if (err) {
+//           return res.status(500).json({ error: err.message });
+//       }
+//       res.json(results);
+//   });
+// });
 
-app.post('/pedido', (req, res) => {
-  const { fecha, id_cliente, fecha_estimada_entrega, fecha_real_entrega } = req.body;
-  connection.query(
-      'INSERT INTO pedido (fecha, id_cliente, fecha_estimada_entrega, fecha_real_entrega) VALUES (?, ?)',
-      [fecha, id_cliente, fecha_estimada_entrega, fecha_real_entrega],
-      (err, results) => {
-          if (err) {
-              return res.status(500).json({ error: err.message });
-          }
-          res.status(201).json({ id: results.insertId });
-      }
-  );
-});
+// app.post('/pedido', (req, res) => {
+//   const { fecha, id_cliente, fecha_estimada_entrega, fecha_real_entrega } = req.body;
+//   connection.query(
+//       'INSERT INTO pedido (fecha, id_cliente, fecha_estimada_entrega, fecha_real_entrega) VALUES (?, ?)',
+//       [fecha, id_cliente, fecha_estimada_entrega, fecha_real_entrega],
+//       (err, results) => {
+//           if (err) {
+//               return res.status(500).json({ error: err.message });
+//           }
+//           res.status(201).json({ id: results.insertId });
+//       }
+//   );
+// });
 
-app.put('/pedido', (req, res) => {
-  const { fecha, id_cliente, fecha_estimada_entrega, fecha_real_entrega } = req.body;
-  connection.query(
-      'UPDATE pedido SET fecha=?, id_cliente=?, fecha_estimada_entrega=?, fecha_real_entrega=?, estado=? WHERE id=?',
-      [fecha, id_cliente, fecha_estimada_entrega, fecha_real_entrega],
-      (err, results) => {
-          if (err) {
-              return res.status(500).json({ error: err.message });
-          }
-          res.status(201).json({ id: results.insertId });
-      }
-  );
-});
+// app.put('/pedido', (req, res) => {
+//   const { fecha, id_cliente, fecha_estimada_entrega, fecha_real_entrega } = req.body;
+//   connection.query(
+//       'UPDATE pedido SET fecha=?, id_cliente=?, fecha_estimada_entrega=?, fecha_real_entrega=?, estado=? WHERE id=?',
+//       [fecha, id_cliente, fecha_estimada_entrega, fecha_real_entrega],
+//       (err, results) => {
+//           if (err) {
+//               return res.status(500).json({ error: err.message });
+//           }
+//           res.status(201).json({ id: results.insertId });
+//       }
+//   );
+// });
 
-app.get('/productoxpedido', (req, res) => {
-  connection.query('SELECT * FROM productoxpedido', (err, results) => {
-      if (err) {
-          return res.status(500).json({ error: err.message });
-      }
-      res.json(results);
-  });
-});
+// app.get('/productoxpedido', (req, res) => {
+//   connection.query('SELECT * FROM productoxpedido', (err, results) => {
+//       if (err) {
+//           return res.status(500).json({ error: err.message });
+//       }
+//       res.json(results);
+//   });
+// });
 
-app.post('/productoxpedido', (req, res) => {
-  const { fecha, id_cliente, fecha_estimada_entrega, fecha_real_entrega } = req.body;
-  connection.query(
-      'INSERT INTO productoxpedido (id_producto, id_pedido, cantidad) VALUES (?, ?)',
-      [fecha, id_cliente, fecha_estimada_entrega, fecha_real_entrega],
-      (err, results) => {
-          if (err) {
-              return res.status(500).json({ error: err.message });
-          }
-          res.status(201).json({ id: results.insertId });
-      }
-  );
-});
+// app.post('/productoxpedido', (req, res) => {
+//   const { fecha, id_cliente, fecha_estimada_entrega, fecha_real_entrega } = req.body;
+//   connection.query(
+//       'INSERT INTO productoxpedido (id_producto, id_pedido, cantidad) VALUES (?, ?)',
+//       [fecha, id_cliente, fecha_estimada_entrega, fecha_real_entrega],
+//       (err, results) => {
+//           if (err) {
+//               return res.status(500).json({ error: err.message });
+//           }
+//           res.status(201).json({ id: results.insertId });
+//       }
+//   );
+// });
 
-app.put('/productoxpedido', (req, res) => {
-  const { id_producto, id_pedido, cantidad } = req.body;
-  connection.query(
-      'UPDATE productoxpedido SET id_producto=?, id_pedido=?, cantidad=? WHERE id_producto=? AND id_pedido=?',
-      [fecha, id_cliente, fecha_estimada_entrega, fecha_real_entrega],
-      (err, results) => {
-          if (err) {
-              return res.status(500).json({ error: err.message });
-          }
-          res.status(201).json({ id: results.insertId });
-      }
-  );
-});
+// app.put('/productoxpedido', (req, res) => {
+//   const { id_producto, id_pedido, cantidad } = req.body;
+//   connection.query(
+//       'UPDATE productoxpedido SET id_producto=?, id_pedido=?, cantidad=? WHERE id_producto=? AND id_pedido=?',
+//       [fecha, id_cliente, fecha_estimada_entrega, fecha_real_entrega],
+//       (err, results) => {
+//           if (err) {
+//               return res.status(500).json({ error: err.message });
+//           }
+//           res.status(201).json({ id: results.insertId });
+//       }
+//   );
+// });
 
 const PORT = 3000;
 app.listen(PORT, () => {
---
+++ Front/source/codigo.js
@@ -0,0 +1,258 @@
+obtenerClientes();
+
+// function eliminarUsuario(codigo) {
+//     console.log(typeof codigo);
+
+//     axios.delete('http://localhost:3000/data/' + codigo)
+//         .then(respuesta => {
+//             alert("Usuario Eliminado");
+//             obtenerUsuarios();
+//         })
+//         .catch(error => {
+//             console.error('Hubo un problema con la solicitud:', error);
+//         });
+// }
+
+// function agregarUsuario(id, nombre, apellido, edad, ciudad) {
+//     axios.post('http://localhost:3000/data/', {
+//         "id": Number(id),
+//         "nombre": nombre,
+//         "apellido": apellido,
+//         "edad": edad,
+//         "ciudad": ciudad
+//     })
+//         .then(respuesta => {
+//             console.log('Usuario agregado con éxito:', respuesta.data);
+//             obtenerUsuarios();
+//         })
+//         .catch(error => {
+//             console.error('Hubo un problema con la solicitud:', error);
+//         });
+// }
+
+// let id = document.getElementById("idPersona");
+// let idActual = document.getElementById("idActual");
+// let nombre = document.getElementById("nombre");
+// let apellido = document.getElementById("apellido");
+// let edad = document.getElementById("edad");
+// let ciudad = document.getElementById("ciudad");
+
+// function datosFormulario(registroConsultado) {
+//     nombre.value = registroConsultado.nombre
+//     id.value = registroConsultado.id;
+//     idActual.value = registroConsultado.id;
+//     apellido.value = registroConsultado.apellido;
+//     edad.value = registroConsultado.edad;
+//     ciudad.value = registroConsultado.ciudad;
+// }
+
+// function habilitarFormulario() {
+//     nombre.disabled = false;
+//     id.disabled = true;
+//     apellido.disabled = false;
+//     edad.disabled = false;
+//     ciudad.disabled = false;
+// }
+
+// function consultarDatos(registroConsultado) {
+//     datosFormulario(registroConsultado)
+//     nombre.disabled = true;
+//     id.disabled = true;
+//     apellido.disabled = true;
+//     edad.disabled = true;
+//     ciudad.disabled = true;
+// }
+
+// function modificarDatos(registroConsultado) {
+//     datosFormulario(registroConsultado)
+//     habilitarFormulario()
+
+// }
+
+function obtenerClientes() {
+    axios.get('http://localhost:3000/cliente')
+        .then(respuesta => {
+
+            let datos = respuesta.data;
+
+            console.log(datos);
+
+
+            let tablaClientes = document.getElementById("tablaClientes");
+            tablaClientes.innerHTML = "";
+
+            for (let indice = 0; indice < datos.length; indice++) {
+                let registro = datos[indice];
+
+                let filaTabla = document.createElement('tr');
+
+                let datoNombre = document.createElement('td');
+                datoNombre.textContent = `${registro.nombre} ${registro.apellido}`;
+                let datoTelefono = document.createElement('td');
+                datoTelefono.textContent = registro.telefono;
+
+                let datoPiso
+                let datoDepartamento
+
+                if (registro.piso !== null) {
+                    console.log(registro.piso)
+                    datoPiso = ` ${registro.piso}`
+                } else {
+                    datoPiso = ""
+                }
+
+                if (registro.departamento !== null) {
+                    console.log(registro.departamento)
+                    datoDepartamento = registro.departamento
+                } else {
+                    datoDepartamento = ""
+                }
+
+                let datoDireccion = document.createElement('td');
+                datoDireccion.textContent = `${registro.calle} ${registro.numero_dir}${datoPiso}${datoDepartamento}`;
+                let datoBarrio = document.createElement('td');
+                datoBarrio.textContent = registro.nombre_barrio;
+                let datoLocalidad = document.createElement('td');
+                datoLocalidad.textContent = registro.nombre_localidad;
+                let datoEmail = document.createElement('td');
+                datoEmail.textContent = registro.correo_electronico;
+                let datoTipoCliente = document.createElement('td');
+                datoTipoCliente.textContent = registro.nombre_tipo_cliente;
+
+                // let celdaAcciones = document.createElement('td');
+
+                // let botonEliminar = document.createElement('button');
+                // botonEliminar.textContent = "Eliminar";
+                // botonEliminar.classList.add('btn', 'btn-danger', 'btn-sm', 'ms-2', 'me-2');
+                // botonEliminar.addEventListener('click', function () {
+                //     if (window.confirm("¿Desea eliminar el registro?")) {
+                //         eliminarUsuario(registro.id);
+                //     }
+                //     window.location.href = '#titulo'
+                // });
+
+                // let botonConsultar = document.createElement('button');
+                // botonConsultar.textContent = "Consultar";
+                // botonConsultar.classList.add('btn', 'btn-primary', 'btn-sm', 'ms-2', 'me-2');
+                // botonConsultar.addEventListener('click', function () {
+                //     consultarDatos(registro)
+                //     botonGuardar.disabled = true
+                //     botonActualizar.disabled = true
+                //     window.location.href = '#contenedorCarga'
+                // })
+
+                // let botonModificar = document.createElement('button');
+                // botonModificar.textContent = "Modificar";
+                // botonModificar.classList.add('btn', 'btn-secondary', 'btn-sm', 'ms-2', 'me-2');
+                // botonModificar.addEventListener('click', function () {
+                //     modificarDatos(registro)
+                //     botonGuardar.disabled = true;
+                //     botonActualizar.disabled = false;
+                //     window.location.href = '#contenedorCarga'
+                // })
+
+                // celdaAcciones.appendChild(botonEliminar);
+                // celdaAcciones.appendChild(botonConsultar);
+                // celdaAcciones.appendChild(botonModificar);
+
+                filaTabla.appendChild(datoNombre);
+                filaTabla.appendChild(datoTelefono);
+                filaTabla.appendChild(datoDireccion);
+                filaTabla.appendChild(datoBarrio);
+                filaTabla.appendChild(datoLocalidad);
+                filaTabla.appendChild(datoEmail);
+                filaTabla.appendChild(datoTipoCliente);
+                // filaTabla.appendChild(celdaAcciones);
+
+                tablaClientes.appendChild(filaTabla);
+            }
+
+        })
+        .catch(error => {
+            console.error('Hubo un problema con la solicitud:', error);
+        });
+}
+
+// let botonGuardar = document.getElementById("botonGuardar");
+
+// botonGuardar.addEventListener('click', function (event) {
+//     event.preventDefault()
+
+//     axios.get('http://localhost:3000/data')
+//         .then(respuesta => {
+
+//             let datos = respuesta.data;
+
+//             let resultado = datos.find(Persona => Persona.id == id.value)
+
+//             if (resultado) {
+//                 alert("El id ya fue ingresado")
+//             } else {
+//                 agregarUsuario(id.value, nombre.value, apellido.value, edad.value, ciudad.value)
+//                 alert("Usuario guardado con éxito")
+//             }
+//         })
+//         .catch(error => {
+//             console.error('Error al obtener los datos:', error);
+//         });
+
+// })
+
+// function modificarUsuario(idActual, id, nombre, apellido, edad, ciudad) {
+
+//     axios.put('http://localhost:3000/data/' + idActual, {
+//         "id": Number(id),
+//         "nombre": nombre,
+//         "apellido": apellido,
+//         "edad": edad,
+//         "ciudad": ciudad
+//     })
+//         .then(respuesta => {
+//             console.log('Usuario modificado con éxito:', respuesta.data);
+//             alert("Usuario modificado con éxito");
+//             obtenerUsuarios();
+//         })
+//         .catch(error => {
+//             console.error('Hubo un problema con la solicitud:', error);
+//         });
+// }
+
+// let botonActualizar = document.getElementById("botonActualizar");
+
+// botonActualizar.addEventListener('click', function (event) {
+//     event.preventDefault()
+
+//     if (idActual.value == id.value) {
+//         modificarUsuario(idActual.value, id.value, nombre.value, apellido.value, edad.value, ciudad.value)
+//     }
+//     else {
+//         axios.get('http://localhost:3000/data')
+//             .then(respuesta => {
+
+//                 let datos = respuesta.data;
+
+//                 let resultado = datos.find(Persona => Persona.id == id.value)
+
+//                 if (resultado) {
+//                     alert("El id ya fue ingresado")
+//                 } else {
+//                     modificarUsuario(idActual.value, id.value, nombre.value, apellido.value, edad.value, ciudad.value)
+//                 }
+//             })
+//             .catch(error => {
+//                 console.error('Error al obtener los datos:', error);
+//             });
+
+//     }
+//     window.location.href = '#titulo'
+
+// })
+
+// let botonLimpiar = document.getElementById('botonLimpiar');
+
+// botonLimpiar.addEventListener('click', function () {
+//     habilitarFormulario()
+//     botonGuardar.disabled = false;
+//     botonActualizar.disabled = true;
+//     id.disabled = false;
+// })
