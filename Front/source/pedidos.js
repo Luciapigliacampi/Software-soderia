54d82adc9fa5b8f501485e225be3a01c46535ce8
Lucia Pigliacampi <lucia.pigliacampi90@gmail.com>
2024-10-14 18:58:30 -0300

    pedidos

--- Front/source/pedidos.js
+++ Front/source/pedidos.js
@@ -22,74 +22,83 @@ function obtenerPedidos() {
                 enlacePedido.href = "#";
                 enlacePedido.setAttribute("data-toggle", "modal")
                 enlacePedido.setAttribute("data-target", "#modal-default")
-                datoNumeroPedido.textContent = `#${registro.id_pedido}`;
+                enlacePedido.textContent = `#${registro.id_pedido}`;
+                datoNumeroPedido.appendChild(enlacePedido)
                 
-                let datoTelefono = document.createElement('td');
-                datoTelefono.textContent = registro.telefono;
+                let datoCliente = document.createElement('td');
+                datoCliente.textContent = registro.nombre_cliente;
 
-                let datoPiso
-                let datoDepartamento
+                // let datoTotal = document.createElement('td');
+                // datoTotal.textContent = registro.total;
 
-                if (registro.piso !== null) {
-                    console.log(registro.piso)
-                    datoPiso = ` ${registro.piso}`
-                } else {
-                    datoPiso = ""
-                }
+                
 
-                if (registro.departamento !== null) {
-                    console.log(registro.departamento)
-                    datoDepartamento = registro.departamento
-                } else {
-                    datoDepartamento = ""
-                }
+            //     let datoFecha = document.createElement('td');
+            //     datoFecha.textContent = registro.fecha;
 
-                let datoDireccion = document.createElement('td');
-                datoDireccion.textContent = `${registro.calle} ${registro.numero_dir}${datoPiso}${datoDepartamento}`;
-                let datoBarrio = document.createElement('td');
-                datoBarrio.textContent = registro.nombre_barrio;
-                let datoLocalidad = document.createElement('td');
-                datoLocalidad.textContent = registro.nombre_localidad;
-                let datoEmail = document.createElement('td');
-                datoEmail.textContent = registro.correo_electronico;
-                let datoTipoCliente = document.createElement('td');
-                datoTipoCliente.textContent = registro.nombre_tipo_cliente;
-                let acciones = document.createElement('td');
-                let botonEliminar = document.createElement('button');
-                botonEliminar.textContent = "Eliminar";
-                botonEliminar.classList.add('btn', 'btn-danger', 'btn-sm', 'ms-2', 'me-2');
-                botonEliminar.addEventListener('click', function () {
-                    if (window.confirm("¿Desea eliminar el registro?")) {
+            //     let datoPiso
+            //     let datoDepartamento
 
-                        eliminarCliente(registro.id_cliente);
-                    }
-                    // window.location.href = '#titulo'
-                });
+            //     if (registro.piso !== null) {
+            //         console.log(registro.piso)
+            //         datoPiso = ` ${registro.piso}`
+            //     } else {
+            //         datoPiso = ""
+            //     }
 
-                let botonModificar = document.createElement('button');
-                botonModificar.textContent = "Modificar";
-                botonModificar.classList.add('btn', 'btn-secondary', 'btn-sm', 'ms-2', 'me-2');
-                botonModificar.setAttribute("data-toggle", "modal")
-                botonModificar.setAttribute("data-target", "#modal-default")
+            //     if (registro.departamento !== null) {
+            //         console.log(registro.departamento)
+            //         datoDepartamento = registro.departamento
+            //     } else {
+            //         datoDepartamento = ""
+            //     }
 
-                botonModificar.addEventListener('click', function (event) {
-                    obtenerCliente(registro.id_cliente)
+            //     let datoDireccion = document.createElement('td');
+            //     datoDireccion.textContent = `${registro.calle} ${registro.numero_dir}${datoPiso}${datoDepartamento}`;
+            //     let datoBarrio = document.createElement('td');
+            //     datoBarrio.textContent = registro.nombre_barrio;
+            //     let datoLocalidad = document.createElement('td');
+            //     datoLocalidad.textContent = registro.nombre_localidad;
+            //     let datoEmail = document.createElement('td');
+            //     datoEmail.textContent = registro.correo_electronico;
+            //     let datoTipoCliente = document.createElement('td');
+            //     datoTipoCliente.textContent = registro.nombre_tipo_cliente;
+            //     let acciones = document.createElement('td');
+            //     let botonEliminar = document.createElement('button');
+            //     botonEliminar.textContent = "Eliminar";
+            //     botonEliminar.classList.add('btn', 'btn-danger', 'btn-sm', 'ms-2', 'me-2');
+            //     botonEliminar.addEventListener('click', function () {
+            //         if (window.confirm("¿Desea eliminar el registro?")) {
 
-                })
+            //             eliminarCliente(registro.id_cliente);
+            //         }
+            //         // window.location.href = '#titulo'
+            //     });
 
-                acciones.appendChild(botonEliminar);
-                acciones.appendChild(botonModificar);
+            //     let botonModificar = document.createElement('button');
+            //     botonModificar.textContent = "Modificar";
+            //     botonModificar.classList.add('btn', 'btn-secondary', 'btn-sm', 'ms-2', 'me-2');
+            //     botonModificar.setAttribute("data-toggle", "modal")
+            //     botonModificar.setAttribute("data-target", "#modal-default")
+
+            //     botonModificar.addEventListener('click', function (event) {
+            //         obtenerCliente(registro.id_cliente)
+
+            //     })
+
+            //     acciones.appendChild(botonEliminar);
+            //     acciones.appendChild(botonModificar);
 
                 filaTabla.appendChild(datoNumeroPedido);
-                filaTabla.appendChild(datoTelefono);
-                filaTabla.appendChild(datoDireccion);
-                filaTabla.appendChild(datoBarrio);
-                filaTabla.appendChild(datoLocalidad);
-                filaTabla.appendChild(datoEmail);
-                filaTabla.appendChild(datoTipoCliente);
-                filaTabla.appendChild(acciones);
+                filaTabla.appendChild(datoCliente);
+                // filaTabla.appendChild(datoDireccion);
+                // filaTabla.appendChild(datoBarrio);
+                // filaTabla.appendChild(datoLocalidad);
+                // filaTabla.appendChild(datoEmail);
+                // filaTabla.appendChild(datoTipoCliente);
+                // filaTabla.appendChild(acciones);
 
-                tablaClientes.appendChild(filaTabla);
+                tablaPedidos.appendChild(filaTabla);
             }
 
         })
